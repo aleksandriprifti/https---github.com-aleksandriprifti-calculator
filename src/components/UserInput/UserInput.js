02c2f695687cc2f163bcfd0bceae358cad067799
aleksandriprifti <prifti.aleksandri@gmail.com>
2023-08-01 16:16:38 +0200

    Second commit

--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -1,42 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 
-const userInput = () => {
+const UserInput = () => {
+  const [inputs, setInputs] = useState({
+    savings: "",
+  });
+
+  const savingChangeHandler = (event) => {
+    setInputs((prevState) => {
+      return {
+        ...prevState,
+        savings: event.target.value,
+      };
+    });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const investData = {
+      saving: inputs.savings,
+    };
+    console.log(investData);
+
+    setInputs({
+      savings: "",
+    });
+  };
+
   return (
-    <div>
-      <form className="form">
-        <div className="input-group">
-          <p>
-            <label htmlFor="current-savings">Current Savings ($)</label>
-            <input type="number" id="current-savings" />
-          </p>
-          <p>
-            <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-            <input type="number" id="yearly-contribution" />
-          </p>
-        </div>
-        <div className="input-group">
-          <p>
-            <label htmlFor="expected-return">
-              Expected Interest (%, per year)
-            </label>
-            <input type="number" id="expected-return" />
-          </p>
-          <p>
-            <label htmlFor="duration">Investment Duration (years)</label>
-            <input type="number" id="duration" />
-          </p>
-        </div>
-        <p className="actions">
-          <button type="reset" className="buttonAlt">
-            Reset
-          </button>
-          <button type="submit" className="button">
-            Calculate
-          </button>
+    <form onSubmit={submitHandler} className="form">
+      <div className="input-group">
+        <p>
+          <label htmlFor="current-savings">Current Savings ($)</label>
+          <input
+            type="number"
+            id="current-savings"
+            value={inputs.savings}
+            onChange={savingChangeHandler}
+          />
         </p>
-      </form>
-    </div>
+        <p>
+          <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+          <input type="number" id="yearly-contribution" />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label htmlFor="expected-return">
+            Expected Interest (%, per year)
+          </label>
+          <input type="number" id="expected-return" />
+        </p>
+        <p>
+          <label htmlFor="duration">Investment Duration (years)</label>
+          <input type="number" id="duration" />
+        </p>
+      </div>
+      <p className="actions">
+        <button type="reset" className="buttonAlt">
+          Reset
+        </button>
+        <button type="submit" className="button">
+          Calculate
+        </button>
+      </p>
+    </form>
   );
 };
 
-export default userInput;
+export default UserInput;
